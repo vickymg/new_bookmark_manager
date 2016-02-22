f6f6f4e6826ec3d584271ff4555bed801a8b763d
Vicky Gray <vickygray26@hotmail.com>
2016-02-22 12:47:24 +0000

    user can add multiple tags to a new link

--- app/app.rb
+++ app/app.rb
@@ -15,10 +15,10 @@ class BookmarkManager < Sinatra::Base
   end
 
   post '/links' do
-    link = Link.new(title: params[:title],
-                    url: params[:url])
-                    tag = Tag.create(name: params[:tags])
-    link.tags << tag
+    link = Link.new(title: params[:title],url: params[:url])
+    params[:tags].split(', ').each do |tag|
+      link.tags << Tag.create(name: tag)
+    end
     link.save
     redirect to('/links')
   end
--- spec/adding_tags_spec.rb
+++ spec/adding_tags_spec.rb
@@ -10,4 +10,14 @@ feature 'Adding tags' do
     expect(link.tags.map(&:name)).to include('news')
   end
 
+  scenario 'I can add multiple tags to a new link' do
+    visit '/links/new'
+    fill_in 'url', with: 'http://www.makersacademy.com'
+    fill_in 'title', with: 'Makers Academy'
+    fill_in 'tags', with: 'education, ruby'
+    click_button 'Create link'
+    link = Link.first
+    expect(link.tags.map(&:name)).to include('education', 'ruby')
+  end
+
 end
