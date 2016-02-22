82a6254401824d06bc8ed5bae6734550fa977001
Vicky Gray <vickygray26@hotmail.com>
2016-02-22 14:32:02 +0000

    user must confirm password

--- spec/user_management_spec.rb
+++ spec/user_management_spec.rb
@@ -1,7 +1,13 @@
 feature 'User sign up' do
+
   scenario 'new user can sign up' do
     expect { sign_up }.to change(User, :count).by(1)
     expect(page).to have_content("Welcome, example@example.com")
     expect(User.first.email).to eq("example@example.com")
   end
+
+  scenario 'user must confirm password to proceed' do
+   expect { sign_up(password_confirmation: '123456' ) }.not_to change(User, :count)
+ end
+
 end
--- spec/web_helper.rb
+++ spec/web_helper.rb
@@ -1,7 +1,9 @@
-def sign_up
-  visit '/users/new'
-  expect(page.status_code).to eq(200)
-  fill_in :email, with: 'example@example.com'
-  fill_in :password, with: 'pa$$word'
-  click_button 'Sign up'
+def sign_up(email: 'example@example.com',
+             password: 'pa$$word',
+             password_confirmation: 'pa$$word')
+    visit '/users/new'
+    fill_in :email, with: email
+    fill_in :password, with: password
+    fill_in :password_confirmation, with: password_confirmation
+    click_button 'Sign up'
 end
